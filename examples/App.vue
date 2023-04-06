31f9dbde9b5d318ba3a0fbd078a65856dcda7d59
答案45 <gdp45@163.com>
2023-04-06 22:59:50 +0800

    配置组件库打包

---
+++ examples/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <gdp-button type="primary">按钮</gdp-button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+</style>
--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import GdpUI from '../packages'
 
 Vue.config.productionTip = false
+Vue.use(GdpUI)
 
 new Vue({
   render: h => h(App)
